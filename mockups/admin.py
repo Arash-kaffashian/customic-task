6756a01abc86f9bb3c06f9921e2d7f3518ffc7e7
Billy Everyteen <kaffashian.arash@gmail.com>
2025-11-28 15:39:54 +0330

    admin panel configured

--- mockups/admin.py
+++ mockups/admin.py
@@ -1,1 +1,21 @@
-# futures : admin panel must be config
+from django.contrib import admin
+from .models import Shirt, Color, Font
+
+
+@admin.register(Shirt)
+class ShirtAdmin(admin.ModelAdmin):
+    list_display = ['color', 'existence', 'image']
+    list_filter = ['color', 'existence']
+    search_fields = ['color', 'existence']
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['color']
+
+
+@admin.register(Font)
+class FontAdmin(admin.ModelAdmin):
+    list_display = ['name', 'font']
+    list_filter = ['name']
+    search_fields = ['name']
