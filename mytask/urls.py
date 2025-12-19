37fb5dafc6234468ebe3cb893d0aea83a0a9801c
Billy Everyteen <kaffashian.arash@gmail.com>
2025-12-19 12:36:01 +0330

    Swagger

--- mytask/urls.py
+++ mytask/urls.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
 
 urlpatterns = [
     path('', include('mockups.urls')),
     path('admin/', admin.site.urls),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema')),
 ]
 
 # WITH THIS CONFIG YOU CAN READ FILES LIKE IMAGES IN BROWSER WHILE THE DEBUG IS TRUE
